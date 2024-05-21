296a9b1abb6f624ebecf6f605dd52a15a30dbefd
unknown <david.valencia-c@labinfo.is.escuelaing.edu.co>
2024-05-21 15:50:03 -0500

    first commit

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Traffic
+                          Cosecha
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">
                           350,897
@@ -66,7 +66,7 @@ const Header = () => {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          New users
+                          Porcentaje de cosecha
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">2,356</span>
                       </div>
@@ -94,7 +94,7 @@ const Header = () => {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Sales
+                          Rendimiento
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">924</span>
                       </div>
@@ -122,7 +122,7 @@ const Header = () => {
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          Performance
+                          Plan
                         </CardTitle>
                         <span className="h2 font-weight-bold mb-0">49,65%</span>
                       </div>
