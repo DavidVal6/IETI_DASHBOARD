f9adfad694238a20223644989ec1efa3b36db872
nicolas.ariza241@gmail.com <nicolas.ariza241@gmail.com>
2024-05-21 21:02:47 -0500

    Avance diseño 1

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -63,10 +63,10 @@ const Auth = (props) => {
             <div className="header-body text-center mb-7">
               <Row className="justify-content-center">
                 <Col lg="5" md="6">
-                  <h1 className="text-white">Welcome!</h1>
+                  <h1 className="text-white">¡Estas a un paso de incrementar tu rendimiento!</h1>
                   <p className="text-lead text-light">
-                    Use these awesome forms to login or create new account in
-                    your project for free.
+                    Agro-gnome le da la bienvenida a todos los clientes que han decidido confiar en nosotros como la mejor
+                    solución para el agro Colombiano
                   </p>
                 </Col>
               </Row>
--- src/views/Index.js
+++ src/views/Index.js
@@ -31,7 +31,6 @@ import {
   NavItem,
   NavLink,
   Nav,
-  Progress,
   Table,
   Container,
   Row,
